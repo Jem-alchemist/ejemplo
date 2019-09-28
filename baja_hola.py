b928626c10a991d4289874a1a08d5a8e91390e41
Jesús Escobar <55922440+Jem-alchemist@users.noreply.github.com>
2019-09-28 22:24:54 +0200

    Update baja_hola.py

--- baja_hola.py
+++ baja_hola.py
@@ -1,6 +1,6 @@
 import urequests
 url_base ='https://raw.githubusercontent.com/Jem-alchemist/ejemplo/master/'                                                                
 r = urequests.get(url_base + 'hola.py')                                              
-f = open('data.txt', 'w')
+f = open('hola.py', 'w')
 f.write('hola.py')
 f.close()
